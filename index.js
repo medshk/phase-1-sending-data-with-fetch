10801eedee1f6818531b30b8a65cfdd21b38c731
med <mohammed.bennaceur98@gmail.com>
2023-09-04 13:38:31 +0100

    success

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+const submitData = (name, email) => {
+  const formData = {
+    name,
+    email,
+  };
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      document.body.append(object.id);
+    })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      document.body.append(error.message);
+    });
+};
+
+submitData("Jack", "jack@gmail.com");
+console.log("hereeeee");
